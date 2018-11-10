13c2afb81aa231779b2be564ae31931b1d82e3fa
pedro_hermosilla <pit2500@gmail.com>
2018-11-10 17:32:21 +0100

    Occlusion sampling protocol: Default resolution is now equal to the one used on the tests of the paper.

--- utils/DataSet.py
+++ utils/DataSet.py
@@ -550,7 +550,7 @@ class DataSet(with_metaclass(abc.ABCMeta)):
 
 
     def _non_uniform_sampling_occlusion_(self, viewDir, points, normals, inNumPoints, inFeatures=None, 
-        inLabels=None, numPoints=0, screenResolution=64):
+        inLabels=None, numPoints=0, screenResolution=128):
         """Method to non-uniformly sample a point cloud using the occlusion protocol. A point is selected
         if it is visible from the view direction.
 
